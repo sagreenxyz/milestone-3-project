62c48d32e39a479b6b7d81aa67cf28c4130585a5
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-17 18:47:38 -0500

    setting up seed.js to truncate equipment and user tables

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -2,22 +2,22 @@ const users = require('./gen-users')
 const equipment = require('./gen-equipment')
 
 // console.log(users(4))
-console.log(equipment(4))
+// console.log(equipment(4))
 
+const { PrismaClient } = require('@prisma/client');
 
-// const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-// const prisma = new PrismaClient();
+const load = async () => {
+    try {
+        await prisma.$queryRaw`TRUNCATE TABLE "equipment" RESTART IDENTITY CASCADE`
+        await prisma.$queryRaw`TRUNCATE TABLE "user" RESTART IDENTITY CASCADE`
+    } catch (e) {
+        console.error(e)
+        process.exit(1)
+    } finally {
+        await prisma.$disconnect()
+    }
+}
 
-// const load = async () => {
-//     try {
-
-//     } catch (e) {
-//         console.error(e)
-//         process.exit(1)
-//     } finally {
-//         await prisma.$disconnect()
-//     }
-// }
-
-// load()
+load()
