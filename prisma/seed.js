ed016ff4705c8009e4dc306d54e0b79235c83c0c
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-18 18:40:23 -0500

    seeder file working, type module still required to seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,13 +1,19 @@
 import { PrismaClient } from '@prisma/client';
-import {faker} from "@faker-js/faker";
+import faker from "faker";
 
 const prisma = new PrismaClient();
+
+const categories = [
+    "Air Compressor And Air Tools",
+    "Heating, Venilation, And Air Conditioning",
+    "Lawn, Landscape, And Tree",
+]
 
 async function main() {
     await prisma.equipment.deleteMany();
     await prisma.user.deleteMany();
 
-    for (let i=0; i<5; i++) {
+    for (let i=0; i<20; i++) {
         await prisma.user.create({
             data: {
                 email: `${faker.internet.email()}`,
@@ -16,34 +22,14 @@ async function main() {
                     create: [
                         {   
                             name: faker.commerce.productName(),
-                            model: faker.random.numeric(5),
+                            model: faker.datatype.string(5),
                             manufacturer: faker.name.lastName(),
                             price: faker.commerce.price(0, 200, 0, '$'),
                             image: faker.image.abstract(250, 250, true),
-                            category: "Air Compressor And Air Tools",
+                            category: categories[Math.floor(Math.random() * categories.length)],
                             isAvailable: false,
                             description: faker.lorem.lines(3),
-                        },
-                        {       
-                            name: faker.commerce.productName(),
-                            model: faker.random.numeric(5),
-                            manufacturer: faker.name.lastName(),
-                            price: faker.commerce.price(0, 200, 0, '$'),
-                            image: faker.image.cats(480, 480, true),
-                            category: "Heating, Venilation, And Air Conditioning",
-                            isAvailable: false,
-                            description: faker.lorem.lines(3),
-                        },
-                        {
-                            name: faker.commerce.productName(),
-                            model: faker.random.numeric(5),
-                            manufacturer: faker.name.lastName(),
-                            price: faker.commerce.price(0, 200, 0, '$'),
-                            image: faker.image.image(480, 480, true),
-                            category: "Lawn, Landscape, And Tree",
-                            isAvailable: false,
-                            description: faker.lorem.lines(3),
-                        },
+                        }
                     ],
                 },
             },
