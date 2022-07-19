4aac77766f76b975a7c71f5d34589687a4e3140c
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-18 19:17:36 -0500

    working on sideNav and Card updates

--- components/Card.js
+++ components/Card.js
@@ -8,14 +8,9 @@ export default function Card(props) {
                 <img class="mx-auto" style={{height: "250px",width:"250px",}} src={props.equipment.image} alt="Sunset in the mountains"></img>
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">{props.equipment.name}</div>
-                    <p class="text-gray-700 text-base">
-                        {props.equipment.description}
-                    </p>
                     <p class="text-sm text-gray-700 text-base">
                         Manufacturer:  {props.equipment.manufacturer} <br/>
                         Model:  {props.equipment.model} <br/>
-                        Price:  {props.equipment.price} <br/>
-                        Category: {props.equipment.category} <br/>
                     </p>
                 </div>
                 <div class="px-6 pt-4 pb-2">
