d743477139ea5de0f923e8698b3785529bd201b1
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-19 00:16:52 -0500

    wrapping Component inside SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <Layout>
-        <Component {...pageProps} />
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </Layout>
     </Provider>
   )
