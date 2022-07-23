cce072a0ecee7b368c427f5e78b93547a9eb0d48
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-23 13:55:16 -0500

    userID did not match ID in database

--- pages/api/equipment/new.js
+++ pages/api/equipment/new.js
@@ -16,19 +16,19 @@ export default async function handler(req, res) {
           image: "https://loremflickr.com/200/200/cats",
           description: description,
           name: name,
-          //   renter: {
-          //       id: 0,
-          //       name: null,
-          //       email: null,
-          //       emailVerified: null,
-          //       image: null,
-          //   },
-          userId: "cl5t0vlyc00048gsocxwdqqw7",
+        //   renter: {
+        //       id: 0,
+        //       name: null,
+        //       email: null,
+        //       emailVerified: null,
+        //       image: null,
+        //   },
+          userId: "cl5t0vnz100398gsoxam1d7ds"
         },
       });
       res.status(200).json(createdEquipment);
     } catch (err) {
-      console.log(err);
+        console.log(err)
       res.status(500).json({ error: `could not add equipment` });
     }
 }
