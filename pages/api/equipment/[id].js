6bd31759b498826be4fe93bcfd5fd46e6310e3b2
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-23 11:10:44 -0500

    cleaning up formatting

--- pages/api/equipment/[id].js
+++ pages/api/equipment/[id].js
@@ -1,38 +1,38 @@
-import prisma from '../../../lib/prisma'
+import prisma from "../../../lib/prisma";
 
 // const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
+  const { id } = req.query;
 
-    const { id } = req.query
-
-    if (req.method === 'DELETE') {
-        const deletedEquipment = await prisma.equipment.delete({
-            where: { id : parseInt(id)}
-        })
-        res.status(200).json(deletedEquipment)
-    } else if (req.method === "PUT") {
-        const { manufacturer, model, price, description } = req.body;
-        const updatedEquipment = await prisma.equipment.update({
-            where: { id : parseInt(id) },
-            data: { 
-                isAvailable: false,
-                manufacturer: manufacturer,
-                model: model,
-                price: price,
-                description: description,
-             },
-        })
-        res.status(200).json(updatedEquipment)
-    } else if (req.method === "GET")
+  if (req.method === "DELETE") {
+    const deletedEquipment = await prisma.equipment.delete({
+      where: { id: parseInt(id) },
+    });
+    res.status(200).json(deletedEquipment);
+  } else if (req.method === "PUT") {
+    const { manufacturer, model, price, description } = req.body;
+    const updatedEquipment = await prisma.equipment.update({
+      where: { id: parseInt(id) },
+      data: {
+        isAvailable: false,
+        manufacturer: manufacturer,
+        model: model,
+        price: price,
+        description: description,
+      },
+    });
+    res.status(200).json(updatedEquipment);
+  } else if (req.method === "GET")
     try {
-        //findUnique not findOne
-        const equipmentFound = await prisma.equipment.findUnique({
-            where: {id: parseInt(id)}
-        })
-        res.status(200).json(equipmentFound)
+      //findUnique not findOne
+      const equipmentFound = await prisma.equipment.findUnique({
+        where: { id: parseInt(id) },
+      });
+      res.status(200).json(equipmentFound);
     } catch (err) {
-        res.status(500).json({error: `could not find data for equipment ID: ${id}`})
+      res
+        .status(500)
+        .json({ error: `could not find data for equipment ID: ${id}` });
     }
-
 }
--- pages/api/equipment/index.js
+++ pages/api/equipment/index.js
@@ -1,11 +1,10 @@
-import prisma from "../../../lib/prisma"
+import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
-
-    try {
-        const equipmentFound = await prisma.equipment.findMany({})
-        res.status(200).json(equipmentFound)
-    } catch (err) {
-        res.status(500).json({error: 'could not find data'})
-    }
+  try {
+    const equipmentFound = await prisma.equipment.findMany({});
+    res.status(200).json(equipmentFound);
+  } catch (err) {
+    res.status(500).json({ error: "could not find data" });
+  }
 }
